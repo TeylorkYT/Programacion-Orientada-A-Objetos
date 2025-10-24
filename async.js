87dbc5c77c3cd4bbc18eaadff36be3e164aab720
TeylorkYT <themalwareisbad@gmail.com>
2025-10-23 21:33:59 -0500

    funciones sincronicas, asincronicas y apis

---
+++ async.js
@@ -0,0 +1,6 @@
+function fetchData() {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
